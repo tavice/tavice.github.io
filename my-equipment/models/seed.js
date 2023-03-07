df06742e380647729e5780da34d1aa03ec4ff621
tavice <thomasavice.ta@gmail.comm>
2023-03-06 17:29:04 -0800

    set up my equipment

---
+++ my-equipment/models/seed.js
@@ -0,0 +1,40 @@
+
+ module.exports = [{
+    year: 2014,
+    description: "Genie Personnel Lift 20",
+    model: "GR-20",
+    purchase: "3\/9\/22",
+    isDriveable: true,
+    location: 'shop',
+    currentUser: 'Bob'
+   },
+   {
+    year: 2014,
+    description: "Genie Personnel Lift 20" ,
+    model: "GR-20",
+    purchase: "3\/9\/22",
+    isDriveable: true,
+    location: 'shop',
+    currentUser: 'Bob'
+   },
+   {
+    year: 2014,
+    description: "Snorkel Scissorlift 10', 30",
+    model: "S3010E",
+    purchase: "3\/9\/22",
+    isDriveable: false,
+    location: 'onsite',
+    currentUser: 'Max'
+
+    
+   },
+   {
+    year: 2016,
+    description: "Genie Personnel Lift 20",
+    model: "GR-20",
+    purchase: "3\/9\/22",
+    isDriveable: false,
+    location: 'onsite',
+    currentUser: 'Max'
+    
+   }]
